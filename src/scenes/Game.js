8d991a97121d3b140fd777f75211b8a3cca9a06a
Will <w.marino997@gmail.com>
2020-08-12 18:25:23 -0400

    update

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,7 @@ import mp3 from "../assets/Orbital\ Colossus.mp3";
 import background from "../assets/scifi_platform_BG1.jpg";
 import tiles from "../assets/scifi_platformTiles_32x32.png";
 import star from "../assets/star.png"
+import platform from "../assets/platform.png";
 import { accelerate, decelerate } from "../utils";
 
 let box;
@@ -17,47 +18,98 @@ export default new Phaser.Class({
   preload: function preload() {
     this.load.image("background", background);
 
-    this.load.spritesheet('tiles', tiles, {
-      frameWidth: 32,
-      frameHeight: 32
-    });
+    // this.load.spritesheet('tiles', tiles, {
+    //   frameWidth: 32,
+    //   frameHeight: 32
+    // });
+
+    // this.load.image("knight", knight)
 
     this.load.image("star", star);
+    this.load.image("platform", platform);
   },
   create: function create() {
     this.add.image(400, 300, "background");
+    let plat1 = this.add.image(400, 300, "platform");
+    plat1.scaleX = 0.2;
+    plat1.scaleY = 0.2;
+    // plat2 = this.add.image(400, 300, "platform");
+    // plat3 = this.add.image(400, 300, "platform");
+    // plat4 = this.add.image(400, 300, "platform");
 
-    const stars = this.physics.add.group({
-      key: 'star',
-      repeat: 0,
-      setScale: {x: 0.2, y: 0.2 },
-      setXY: { x:400, y: 300 }
-    });
 
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-      child.setVelocityX(150 - Math.random() * 300);
-      child.setVelocityY(150 - Math.random() * 300);
-      child.setBounce(1, 1);
-      child.setCollideWorldBounds(true);
-    });
+    // const stars = this.physics.add.group({
+    //   key: 'star',
+    //   repeat: 5,
+    //   setScale: { x: 0.2, y: 0.2 },
+      // setXY: { x:400, y: 300 }
+    // });
+    // debugger;
+
+
+    // stars.children.iterate(function (child) {
+    //   debugger;
+    //   // child.setXY(400, 300)
+    //   // child.body.x = 400
+    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    //   child.setVelocityX(150 - Math.random() * 300);
+    //   child.setVelocityY(150 - Math.random() * 300);
+    //   child.setBounce(1, 1);
+    //   child.setCollideWorldBounds(true);
+    // });
+
+    // let firstX = 100;
+    // for(let i = 0; i < stars.children.entries.length; i++){
+      // let curChild = stars.children.entries[i];
+      // curChild.setX(400, 300);
+      // curChild.body.x = 100 + (i * 50);
+      // curChild.body.y = 100;
+      // curChild.body.x = 400;
+      // curChild.body.y = 400;
+      // curChild.setVelocityX(0);
+      // curChild.setAccelerationY(-1000);
+      // curChild.setVelocityY(0);
+      // curChild.body.x = firstX + (i * 50);
+      // curChild.body.y = 100;
+      // curChild.body.velocity.y = -1000;
+    // }
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    box = this.physics.add.image(400, 100, "tiles", 15);
 
-    const processCollision = (box, star) => {
-      star.destroy();
-      const starsLeft = stars.countActive();
-      if (starsLeft === 0) {
-        this.scene.start('winscreen');
-      }
+    // box = this.physics.add.image(400, 100, "tiles", 15);
+    box = this.physics.add.image(400, 100, "knight", 15);
+
+
+    // const processCollision = (box, star) => {
+    //   star.destroy();
+    //   const starsLeft = stars.countActive();
+    //   if (starsLeft === 0) {
+    //     this.scene.start('winscreen');
+    //   }
+    // }
+
+    // this.physics.add.collider(
+    //   stars,
+    //   box,
+    //   processCollision,
+    //   null,
+    //   this
+    // );
+
+    const platformProcessCollision = (box, platform) => {
+      // star.destroy();
+      // const starsLeft = stars.countActive();
+      // if (starsLeft === 0) {
+        // this.scene.start('winscreen');
+      // }
+      box.body.acceleration.y = -1000;
     }
 
     this.physics.add.collider(
-      stars,
+      platforms,
       box,
-      processCollision,
+      platformProcessCollision,
       null,
       this
     );
@@ -70,28 +122,38 @@ export default new Phaser.Class({
   update: function () {
     const { velocity } = box.body;
     box.has_jumped = false;
+    let upArrow = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    debugger;
 
-    if (cursors.space.isDown) {
-      const x = decelerate(velocity.x);
-      const y = decelerate(velocity.y);
-      box.setVelocity(x, y)
-    }
+    // if (cursors.space.isDown) {
+    //   const x = decelerate(velocity.x);
+    //   const y = decelerate(velocity.y);
+    //   box.setVelocity(x, y)
+    // }
+
+    // if(Phaser.Input.Keyboard.JustDown(spacebar)){
+    //   velocity.y = -300;
+    // }
 
 
     // if (cursors.up.isDown) box.setVelocityY(accelerate(velocity.y, -1));
-    if (cursors.up.isDown){
-      if(!box.has_jumped){
-        velocity.y = -300;
-        box.has_jumped = true;
-      }
-    };
+    // if (cursors.up.isDown){
+    //   if(!box.has_jumped){
+    //     velocity.y = -300;
+    //     box.has_jumped = true;
+    //   }
+    // };
+
+    if(Phaser.Input.Keyboard.JustDown(upArrow)){
+      velocity.y = -450;
+    }
 
     if (cursors.right.isDown) {
       if(box.body.velocity.x < 0){
         box.body.velocity.x = 0
       }
       box.setVelocityX(accelerate(velocity.x, 2));
-      console.log(box.body.velocity.x);
+      // console.log(box.body.velocity.x);
     };
 
     if (cursors.down.isDown) box.setVelocityY(accelerate(velocity.y, 2));
@@ -104,11 +166,16 @@ export default new Phaser.Class({
       box.setVelocityX(accelerate(velocity.x, -2));
 
       if(box.body.velocity.x < -300){
-        debugger;
         box.body.velocity.x = -300;
       }
-      console.log(box.body.velocity.x);
+      // console.log(box.body.velocity.x);
     };
+
+    if(box.body.velocity.x > 0){
+      box.body.velocity.x -= 5;
+    }else if(box.body.velocity.x < 0){
+      box.body.velocity.x += 5;
+    }
 
   }
 });
